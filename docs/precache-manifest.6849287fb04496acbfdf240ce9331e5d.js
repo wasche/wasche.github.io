8eeebafa9e65db2d7237160a6ea0b2209251da4c
Wil Asche <wil@asche.us>
2019-09-20 16:43:41 -0400

    convert to react app

---
+++ docs/precache-manifest.6849287fb04496acbfdf240ce9331e5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81a92b17d742693f3d61bff39580bd61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aec181c7ef0f4602cf2",
+    "url": "/static/css/main.26bf9620.chunk.css"
+  },
+  {
+    "revision": "e823578753edae0a19c6",
+    "url": "/static/js/2.30f41ff5.chunk.js"
+  },
+  {
+    "revision": "5aec181c7ef0f4602cf2",
+    "url": "/static/js/main.68b6abec.chunk.js"
+  },
+  {
+    "revision": "db116b4b394c81eb9c41",
+    "url": "/static/js/runtime-main.eaf29904.js"
+  }
+]);
